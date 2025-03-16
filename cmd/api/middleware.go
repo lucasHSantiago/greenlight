4770b14905c783153e8ec9032b1921861c1435e5
lucasHSantiago <lucashenriquesantiago@gmail.com>
2025-03-16 19:45:52 -0300

    endpoint metrics

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"errors"
+	"expvar"
 	"fmt"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
 
+	"slices"
+
+	"github.com/felixge/httpsnoop"
 	"github.com/lucasHSantiago/greenlight/internal/data"
 	"github.com/lucasHSantiago/greenlight/internal/validator"
 	"golang.org/x/time/rate"
-	"slices"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -197,5 +201,24 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 		}
 
 		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) metrics(next http.Handler) http.Handler {
+	totalRequestsReceived := expvar.NewInt("total_requests_received")
+	totalResponsesSent := expvar.NewInt("total_responses_sent")
+	totalProcessingTimeMicroseconds := expvar.NewInt("total_processing_time_ms")
+	totalResponsesSentByStatus := expvar.NewMap("total_responses_sent_by_status")
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		totalRequestsReceived.Add(1)
+
+		metrics := httpsnoop.CaptureMetrics(next, w, r)
+
+		totalResponsesSent.Add(1)
+
+		totalProcessingTimeMicroseconds.Add(metrics.Duration.Microseconds())
+
+		totalResponsesSentByStatus.Add(strconv.Itoa(metrics.Code), 1)
 	})
 }
